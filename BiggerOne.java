5120a3260a337b4a1dc988516bda89d2db3beecb
Sujay Syal <52260748+sujaysyal@users.noreply.github.com>
2021-04-15 21:44:43 -0700

    Update BiggerOne.java

--- BiggerOne.java
+++ BiggerOne.java
@@ -1,1 +1,45 @@
+import java.util.Scanner;
 
+public class Main{
+
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+
+    int num_1;
+    int num_2;
+
+    System.out.println("Enter a number: ");
+    num_1 = input.nextInt();
+    System.out.println("Enter another number: ");
+    num_2 = input.nextInt();
+
+    int answer = isBigger(num_1, num_2);
+    System.out.println(answer);
+
+  }
+
+  public static int isBigger(int num1, int num2){
+
+
+    if (num1 > num2){
+
+      return num1;
+
+    }
+
+    else if (num1 == num2) {
+
+      return num1;
+
+    }
+
+    else{
+
+      return num2;
+
+    }
+
+  }
+
+}
